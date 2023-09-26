707b1df99131eb1c520a817c8b0c4640bbc734f9
thrkrdk <thrkardak@gmail.com>
2023-09-26 10:10:29 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# SolidPrinciplesInAngular
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.0.3.
+
+## Development server
+
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+
+## How to use
+
+This project has 5 branches where every branch has implementation of some certain design principle.
+Please visit [This video tutorial](https://youtu.be/Y-MRJ9QYCvI) to see better explenation of SOLID principles.
---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button'; 
+
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatProgressBarModule,
+    MatToolbarModule,
+    MatDividerModule,
+    MatButtonModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
